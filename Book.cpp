3cce8d617e2d33e2a4aaf37d1925d62fcf9c578c
Sharaa_re <norouzi.sharare@gmail.com>
2020-04-22 13:27:22 +0430

    add Book class

--- Book.cpp
+++ Book.cpp
@@ -1,11 +1,15 @@
 #include "Book.h"
 #include "parser.h"
+
+#include <iostream>
+
 using namespace std;
 Book::Book(vector<string> fields) {
     id = stoi(fields[BOOK_ID]);
     title = fields[BOOK_TITLE];
     genre_1 = fields[GENRE_1];
     genre_2 = fields[GENRE_2];
+    pages = stoi(fields[PAGES]);
     author = fields[AUTHOR_NAME];
     author_average_rating = stof(fields[AUTHOR_AVERAGE_RATING]);
 }
@@ -17,4 +21,17 @@ void Book::add_review(int rate, int num) {
 
 float Book::rate(){
     return (author_average_rating + (reviews_sum / reviews_likes_count)) / 10;
+}
+
+ostream& operator<<(ostream& os, const Book& bk) {
+    os << "id: " << bk.id << endl;
+    os << "Title: " << bk.title << endl;
+    os << "Genres: " << bk.genre_1 << ", " << bk.genre_2 << endl;
+    os << "Number of Pages: " << bk.pages << endl;
+    os << "Author: " << bk.author << endl;
+    os << "Author Rating: " << bk.author_average_rating << endl;
+
+    return os;
+    
+
 }
--- Book.h
+++ Book.h
@@ -8,7 +8,7 @@
 
 class Book
 {
-private:
+protected:
     int id;
     std::string title;
     std::string genre_1;
@@ -19,11 +19,13 @@ private:
     int reviews_sum;
     int reviews_likes_count;
 
+
 public:
     Book(std::map<std::string, std::string> fields);
     Book(std::vector<std::string> fields);
     void add_review(int rate, int number_of_likes);
     float rate();
+    friend std::ostream& operator<<(std::ostream& os, const Book& bk);
 };
 
 
