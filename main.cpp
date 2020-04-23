6ab629d55b9ebcb5fcf62330a1bdf71e92a3fdbc
Sharaa_re <norouzi.sharare@gmail.com>
2020-04-23 15:10:55 +0430

    comment time from main

--- main.cpp
+++ main.cpp
@@ -11,17 +11,17 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    const clock_t begin_time = clock();
+    // const clock_t begin_time = clock();
 
     if (argc < 2){
         cerr << "Few Arguments for Program!" << endl;
         exit(EXIT_FAILURE);
     } 
-
     string genre(argv[1]);
     cout << Goodreads().find_fav_book(genre);
-    // cout << "ehem";
-    cout << float(clock() - begin_time) / CLOCKS_PER_SEC << endl;
+
+    // cout << float(clock() - begin_time) / CLOCKS_PER_SEC << endl;
+    
     return 0;
 
 }
